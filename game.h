a897b60bf499be7626cbb97c54516dc22b4b1903
Jay Coskey <jay.coskey@gmail.com>
2021-03-20 23:50:11 -0800

    Day 2: Move move-related types & funcs to move.h. Add player Move entry/parsing & slide/leap funcs.

--- game.h
+++ game.h
@@ -6,85 +6,131 @@
 #include <tuple>
 #include <vector>
 
+#include "util.h"
+#include "player.h"
+#include "piece.h"
+#include "board.h"
+#include "move.h"
 
-enum CheckType {
-    NotCheck,
-    Check,
-    CheckMate
-};
+using std::cout;
+using std::map;
 
-enum EndState {
-    NotEnd,
+
+enum class GameState {
+    InPlay,
     Tie,
     WinBlack,
     WinWhite
 };
 
-using MoveRule = std::function<std::vector<Move>(Board, Color, Pos)>;
-
-// Some moves (e.g., en passant, castling, Pawn promotion) have multiple "steps".
-// For such moves, an undo operation must undo all components, or none at all.
-// A "primary" move can have an associated "secondary" moves, making a MoveGroup.
-// MoveGroups must be undone atomically, or not at all.
-//
 // TODO: Privacy
 class Game {
-    Game() {
-        initMoveRules();
-    }
+public:
+    Game();
 
-    static std::map<PieceType, MoveRule> pieceType2moveRule;
+    void execMove(Board& board, const Move& move);
+    MoveRule getMoveRule(PieceType pieceType);
+    void play();
 
 private:
-    auto checkGameEnd(const Board& board, const Player& playerCompletingMove) {
-        // TODO: isCheckmate
-        // TODO: isStalemate [multiple forms]
-        bool     isGameOver = false;   // TODO
-        EndState endState   = NotEnd;  // TODO
-        return std::make_tuple(isGameOver, endState);
-    }
-    void initMoveRules() {
-        // TODO: Assert the map has not been initialized
-        pieceType2moveRule[King]         = [&](const auto b, const auto c, const auto pos)
-                                               { return steps(b, c, King, pos, allDirs); };
-        // TODO: pieceType2moveRule[Queen]  = [&](const auto b, const auto c, const auto pos){ return slides(... allDirs); };
-        // TODO: pieceType2moveRule[Rook]   = [&](const auto b, const auto c, const auto pos){ return slides(... orthoDirs); };
-        // TODO: pieceType2moveRule[Bishop] = [&](const auto b, const auto c, const auto pos){ return slides(... diagDirs); };
-        // TODO: pieceType2moveRule[Knight] = [&](const auto b, const auto c, const auto pos){ return leaps(.. knightDirs); };
-        // TODO: pieceType2moveRule[Pawn]   = pawnMoveRule;
-    }
-    std::vector<Move>
-    pawnMoveRule(const Board& b, const Player& p, const Pos& pos) {
-        std::vector<Move> result{};
-        // TODO: if (relRow(pos) < BOARD_ROWS - 1) { result.insert(step(p.getForward())); }  // Advance 1 space
-        // TODO: if (relRow(pos) == 2) { result.insert(step(p.getForward(), 2)); }           // Advance 2 spaces
-        // TODO: if (piece at FL) { result.insert(move + capture); }                // Capture FL
-        // TODO: if (piece at FR) { result.insert(move + capture); }                // Capture FL
-        // TODO: if (is en passant available) { capture en passant }                // Capture FL
-        // TODO: if (relRow(pos) == BOARD_ROWS) { append pawn promotion to each item in result } // Pawn promotion
-        return result;
-    }
+    std::tuple<bool, GameState> getGameState(const Player& playerCompletingMove);
+    void initMoveRules();
+    void initPlayers();
 
-    auto play() {
-        bool     isGameOver = false;
-        EndState endState;
-
-        for (int round_num = 0; ;++round_num) {
-            for (Player player : players) {
-                // TODO: player.getMove()
-                std::tie(isGameOver, endState) = checkGameEnd(board, player);
-                if (endState != NotEnd) { break; }
-            }
-            if (endState != NotEnd) { break; }
-        }
-        // TODO: Announce winner
-    }
-
-    // ----------
     Board board;
+    Players players;
+
+    map<Hash, int> boardLayoutRepetitionCount;
     int moveCount = 0;
     int movesSinceCapture = 0;
-    std::vector<Move> moveHistory;
-    std::map<Hash, int> boardLayoutRepetitions;
-    std::vector<Player> players;
+    Moves moveHistory;
+    PieceType2MoveRule pieceType2MoveRule;
 };
+
+// ---------- Public members
+
+Game::Game()
+{
+    // Init board not needed---use default
+    initMoveRules();
+    initPlayers();
+}
+
+void Game::execMove(Board& board, const Move& move)
+{
+    // TODO: Support complex moves (en passant, castling, Pawn promotion, etc.
+    PieceP movedPieceP = board.removePieceFrom(move.from());
+    board.addPieceTo(movedPieceP, move.to());
+}
+
+MoveRule Game::getMoveRule(PieceType pieceType)
+{
+    return pieceType2MoveRule.at(pieceType);
+}
+
+void Game::play()
+{
+    bool     isGameOver = false;
+    GameState endState;
+
+    for (int round_num = 0; ;++round_num) {
+        for (auto& player : players) {
+            cout << board << "\n";
+
+            const Pos2Moves& validPlayerMoves = getValidPlayerMoves(pieceType2MoveRule, board, player.color());
+            Move move = getPlayerMove(board, player, validPlayerMoves);
+            execMove(board, move);
+            std::tie(isGameOver, endState) = getGameState(player);
+            if (endState != GameState::InPlay) { break; }
+        }
+        if (endState != GameState::InPlay) { break; }
+    }
+    // TODO: Announce winner
+}
+
+// ---------- private Game members
+
+std::tuple<bool, GameState> Game::getGameState(const Player& playerCompletingMove)
+{
+    // TODO:  isCheckmate
+    // TODO:  isStalemate [multiple forms]
+    bool      isGameOver = false;   // TODO
+    GameState gameState  = GameState::InPlay;  // TODO
+    return std::make_tuple(isGameOver, gameState);
+}
+
+void Game::initMoveRules()
+{
+    // TODO: Assert the map has not been initialized
+    pieceType2MoveRule[PieceType::King]   = [&](const auto& b, const auto c, const auto& pos)
+                                              { return move_steps( b, c, pos, PieceType::King,   allDirs); };
+    pieceType2MoveRule[PieceType::Queen]  = [&](const auto& b, const auto c, const auto& pos)
+                                              { return move_slides(b, c, pos, PieceType::Queen,  allDirs); };
+    pieceType2MoveRule[PieceType::Rook]   = [&](const auto& b, const auto c, const auto& pos)
+                                              { return move_slides(b, c, pos, PieceType::Rook,   orthoDirs); };
+    pieceType2MoveRule[PieceType::Bishop] = [&](const auto& b, const auto c, const auto& pos)
+                                              { return move_slides(b, c, pos, PieceType::Bishop, diagDirs); };
+    pieceType2MoveRule[PieceType::Knight] = [&](const auto& b, const auto c, const auto& pos)
+                                              { return move_leaps( b, c, pos, PieceType::Knight, knightDirs); };
+    // TODO: pieceType2MoveRule[PieceType::Pawn]   = pawnMoveRule;
+}
+
+void Game::initPlayers()
+{
+    players.emplace(players.end(), Color::White, Dir(0,  1), "Amy");
+    players.emplace(players.end(), Color::Black, Dir(0, -1), "Bob");
+}
+
+// ---------- Game non-member functions
+
+const Moves& pawnMoveRule(const Board& b, const Player& p, const Pos& pos)
+{
+    Moves result{};
+    // TODO: if (relRow(pos) < BOARD_ROWS - 1) { result.insert(step(p.getForward())); }  // Advance 1 space
+    // TODO: if (relRow(pos) == 2) { result.insert(step(p.getForward(), 2)); }           // Advance 2 spaces
+    // TODO: if (piece at FL) { result.insert(move + capture); }                // Capture FL
+    // TODO: if (piece at FR) { result.insert(move + capture); }                // Capture FL
+    // TODO: if (is en passant valid) { capture en passant }                // Capture FL
+    // TODO: if (relRow(pos) == BOARD_ROWS) { append pawn promotion to each item in result } // Pawn promotion
+    return result;
+}
--- move.h
+++ move.h
@@ -2,11 +2,23 @@
 
 #pragma once
 
-#include <vector>
+#include <regex>
 
 #include "util.h"
+#include "geometry.h"
+#include "player.h"
 #include "piece.h"
+#include "board.h"
 
+using std::string, std::ostream;
+using std::cin, std::cout, std::ostringstream;
+
+
+enum class CheckType {
+    NotCheck,
+    Check,
+    CheckMate
+};
 
 // Types of moves/move steps/actions in chess:
 //   - Move primary piece
@@ -14,24 +26,248 @@
 //   - Remove captured piece from board (destination of move, except in en passant)
 //   - Exchange piece with another (when promoting a Pawn)
 
+enum class MoveType {
+    Simple,
+    CastleK,
+    CastleQ,
+    EnPassant,
+    Promotion
+};
+
 // TODO: Expand to include MoveSteps, as described below
 // TODO: Privacy
 class Move {
 public:
-    Move(Color color, PieceType type, Pos from, Pos to) : color{color}, type{type}, from{from}, to{to} {}
-    Color color;
-    PieceType type;
-    Pos from;
-    Pos to;
+    Move(Color color, PieceType pieceType, Pos from, Pos to)
+        : _color{color}, _pieceType{pieceType}, _from{from}, _to{to}
+        // TODO: _checkType, _captured, _isPromotion
+        {}
+    Color      color()      const { return _color;       }
+    PieceType  pieceType()  const { return _pieceType;   }
+    const Pos& from()       const { return _from;        }
+    const Pos& to()         const { return _to;          }
+    CheckType  checkType()  const { return _checkType;   }
+
+    const PieceP captured() const { return _captured;    }
+    bool isPromotion()      const { return _isPromotion; }
+
+    bool operator==(const Move& other) const;
+
+private:
+    Color     _color;
+    PieceType _pieceType;
+    Pos       _from;
+    Pos       _to;
+    CheckType _checkType;
+
+    PieceP    _captured;
+    bool      _isPromotion;
+
+    bool operator<(const Move& other) const;
+
+    friend ostream& operator<<(ostream& os, const Move& move);
 };
 
-std::vector<Move> steps(const Board& board, Color color, PieceType type, Pos pos, std::set<Dir> dirs) {
-     std::vector<Move> result{};
-     for (Dir dir : dirs) {
-         Pos dest = pos + dir;
-         if (board.containsPos(dest)) {
-             result.emplace(result.end(), color, type, pos, pos+dir);
-         }
-     }
-     return result;
+ostream& operator<<(ostream& os, const Move& move) {
+    os << "(from=" << move._from << ", to=" << move._to << ')';
+    return os;
+}
+
+bool Move::operator==(const Move& other) const
+{
+    return _color == other._color
+        && _pieceType == other._pieceType
+        && _from == other._from
+        && _to == other._to;
+}
+
+// ---------- private Move members
+
+bool Move::operator<(const Move& other) const
+{
+    if (  _from < other._from
+       || (_from.x == other._from.x && _from.y == other._from.y && _to < other._to)
+       )  { return true; }
+    return false;
+}
+
+// ---------- Move non-member functions
+
+const Move parseMoveInAlgNotation(const Board& board, const string& input);
+
+const Move getPlayerMove(
+    const Board& board
+    , const Player& player
+    , const Pos2Moves& validPlayerMoves)
+{
+    const string& help   = "\tEnter moves as a pair of board positions in algebraic notation. E.g.: e2 e4";
+    const string& prompt = "Enter move (? for help): ";
+    cout << player.name() << "'s turn\n";
+
+    while (true) {
+        cout << prompt;
+        try {
+            string input;
+            getline(cin, input);
+            if (input[0] == '?') {
+                cout << help << "\n";
+                continue;
+            }
+            if (input == "list") {
+                cout << "List of valid moves\n";
+                for (const auto& [from, moves] : validPlayerMoves) {
+                    PieceType pt = board.pieceAt(from)->get()->pieceType();
+                    cout << "  Moves of " << showPieceType(pt)
+                         << '@' <<  from << " (count=" << moves.size() << "):\n";
+                    for (const Move& move : moves) {
+                        cout << "    " << move << "\n";
+                    }
+                }
+                continue;
+            }
+            Move move = parseMoveInAlgNotation(board, input);
+            cout << "Getting valid piece moves: from=" << move.from() << "\n";
+            Moves validPieceMoves = validPlayerMoves.at(move.from());
+            const auto& beg = validPieceMoves.begin();
+            const auto& end = validPieceMoves.end();
+            if (find(beg, end, move) == end) {
+                cout << "That is not a legal move." << "\n";
+                continue;
+            }
+            // TODO: Check to see if player is putting self in check.
+            return move;
+        }
+        catch (const std::invalid_argument& ex) {
+            cout << ex.what();
+        }
+    }
+}
+
+const Pos2Moves getValidPlayerMoves(
+    const PieceType2MoveRule& pieceType2MoveRule
+    , const Board& board
+    , const Color& color
+    )
+{
+    Pos2Moves result;
+    for (const PieceP& pieceP : board.piecesWithColor(color)) {
+        PieceType pt = pieceP->pieceType();
+        MoveRule moveRule = pieceType2MoveRule.at(pt);
+        // cout << "Finding legal moves for " << *pieceP << "\n";
+        for (Move move : moveRule(board, pieceP->color(), pieceP->pos())) {
+            auto& moves = result[pieceP->pos()];
+            moves.insert(moves.end(), move);
+        }
+    }
+    return result;
+}
+
+Moves move_leaps(const Board& board, Color color, const Pos& pos, PieceType pieceType, const Dirs& dirs)
+{
+    bool verbose = false;
+
+    Moves result{};
+    for (const Dir& dir : dirs) {
+        Pos dest(pos + dir);
+        verbose && cout << "leaps(" << showPieceType(pieceType) << "): "
+                        << "Dest test: " << dest << "\n";
+        if (!board.containsPos(dest)) {
+            verbose && cout << "leaps(" << showPieceType(pieceType) << "): "
+                            << "Dest rejected: " << dest << " Not on board\n";
+            continue;
+        }
+        auto oPieceAtDestP = board.pieceAt(dest);
+        if (oPieceAtDestP != std::nullopt) {
+            if (oPieceAtDestP->get()->color() == color) {
+                verbose && cout << "leaps(" << showPieceType(pieceType) << "): "
+                                << "Dest rejected: " << dest << " Piece belongs to same player\n";
+                continue;  // Cannot capture one's own piece
+            }
+        }
+        verbose && cout << "leaps: Dest is legal: " << dest << "\n";
+        result.emplace(result.end(), color, pieceType, pos, pos+dir);
+    }
+    return result;
+}
+
+Moves move_slides( const Board& board, Color color, Pos pos, PieceType pieceType, Dirs dirs
+            , Short maxSteps=100)
+{
+    Moves result{};
+    for (Dir dir : dirs) {
+        Pos dest{pos};
+        for (Short stepCount = 1; stepCount <= maxSteps; ++stepCount) {
+            dest = dest + dir;  // Step in direction dir
+            if (!board.containsPos(dest)) {
+                break;  // Done stepping in this direction
+            }
+            auto oPieceAtDestP = board.pieceAt(dest);
+            if (oPieceAtDestP != std::nullopt) {
+                if (oPieceAtDestP->get()->color() == color) {
+                    break;  // Cannot capture one's own piece
+                }
+                // cout << "INFO: slides: Found capture @ " << dest << " by " << showPieceType(pieceType) << "\n";
+                result.emplace(result.end(), color, pieceType, pos, dest);  // Capture
+                break;  // Cannot move past opponent's piece
+            } else {
+                // cout << "INFO: slides: Found non-capture @ " << dest << " by " showPieceType(pieceType) << "\n";
+                result.emplace(result.end(), color, pieceType, pos, dest);  // No capture
+            }
+        }
+    }
+    return result;
+}
+
+Moves move_steps(const Board& board, Color color, Pos pos, PieceType pieceType, Dirs dirs)
+{
+    return move_leaps(board, color, pos, pieceType, dirs);
+}
+
+const Move parseMoveInAlgNotation(const Board& board, const string& input) noexcept(false)
+{
+    std::regex halvesRegex{"^\\s*(\\S+)\\s+(\\S+)$"};
+    std::regex algNotationRegex{"^\\s*([a-z]+)([1-9][0-9]*)$"};
+
+    std::smatch halvesMatch;
+    Pos *fromP;
+    Pos *toP;
+    if (std::regex_match(input, halvesMatch, halvesRegex)) {
+        if (halvesMatch.size() != 3) {
+            throw new std::invalid_argument("Input has the wrong format");
+        }
+        for (unsigned int k = 1; k < halvesMatch.size(); ++k) {
+            string half = halvesMatch[k];
+            string halfName = k == 1 ? "From" : "To";
+            std::smatch algNotationMatch;
+            if (std::regex_match(half, algNotationMatch, algNotationRegex)) {
+                string colStr = algNotationMatch[1];
+                string rowStr = algNotationMatch[2];
+
+                int col = tolower(colStr[0]) - 'a';
+                if (col < 0 or col >= BOARD_COLS) {
+                    ostringstream oss;
+                    oss << halfName << " position has an illegal column number: " << col;
+                    throw new std::invalid_argument(oss.str());
+                }
+
+                int row = stoi(rowStr);
+                if (row < 1 or row > BOARD_ROWS) {
+                    ostringstream oss;
+                    oss << halfName << " position has an illegal row number: " << row;
+                    throw new std::invalid_argument(oss.str());
+                }
+                row -= 1;  // Adjust for difference between alg chess notation and C++ indexing
+                if (k == 1) { fromP = new Pos(col, row); }
+                else { toP = new Pos(col, row); }
+            } else {
+                ostringstream oss;
+                oss << halfName << " position has the wrong format";
+                throw new std::invalid_argument(oss.str());
+            }
+        }
+    } else {
+        throw new std::invalid_argument("Input has the wrong format");
+    }
+    const Piece& fromPiece = *(board.pieceAt(*fromP)->get());
+    return Move{fromPiece.color(), fromPiece.pieceType(), *fromP, *toP};
 }
