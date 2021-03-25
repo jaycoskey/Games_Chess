052398ad10c6b6c775091c05c142da63f3a812bd
Jay Coskey <jay.coskey@gmail.com>
2021-03-24 23:11:00 -0800

    Day 4: New sanity test of Board class

---
+++ test_board.cpp
@@ -0,0 +1,63 @@
+// Copyright 2021, by Jay M. Coskey
+
+#include <iostream>
+
+#include "util.h"
+#include "piece.h"
+#include "board.h"
+
+using std::cout;
+
+
+void test_board_pieceCounts() {
+    Board board;
+
+    for (const auto& [color, piecePs] : board.color2PiecePs) {
+        cout << "Color = " << showColor(color) << "\n";
+
+        Short kingCount   = 0;
+        Short queenCount  = 0;
+        Short rookCount   = 0;
+        Short bishopCount = 0;
+        Short knightCount = 0;
+        Short pawnCount   = 0;
+
+        for (const PieceP& pieceP : piecePs) {
+            // cout << "Piece = " << *pieceP << "\n";
+
+            switch(pieceP->pieceType()) {
+              case PieceType::King:
+                kingCount++;
+                break;
+              case PieceType::Queen:
+                queenCount++;
+                break;
+              case PieceType::Rook:
+                rookCount++;
+                break;
+              case PieceType::Bishop:
+                bishopCount++;
+                break;
+              case PieceType::Knight:
+                knightCount++;
+                break;
+              case PieceType::Pawn:
+                pawnCount++;
+                break;
+              case PieceType::None:
+                throw new std::invalid_argument("Invalid piece type in board data");
+            }
+        }
+        assert(kingCount   == 1);
+        assert(queenCount  == 1);
+        assert(rookCount   == 2);
+        assert(bishopCount == 2);
+        assert(knightCount == 2);
+        assert(pawnCount   == 8);
+    }
+}
+
+int main() {
+    test_board_pieceCounts();
+    return 0;
+}
