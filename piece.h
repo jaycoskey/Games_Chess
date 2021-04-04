298173ae263092b4936a111a6710fc635c933f95
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:24:57 -0800

    Add moveHistory, primarily for tracking King & Rook (for castling), and Pawn movement (for en passant).

--- piece.h
+++ piece.h
@@ -2,20 +2,26 @@
 
 #pragma once
 
+#include <algorithm>
 #include <map>
 
 #include "util.h"
 #include "geometry.h"
 // #include "player.h"
 
-using std::string;
+#include "logger.h"
+
 using std::ostream;
 using std::map, std::vector;
 
-using PieceValue = float;
+using MoveIndex         = Short;
+using MoveIndexHistory  = vector<bool>;
+using PieceValue        = float;
 
+const PieceValue KING_VALUE = 1'000.0;
+
+// ========================================
 enum class PieceType {
-    None,
     King,
     Queen,
     Rook,
@@ -23,6 +29,13 @@ enum class PieceType {
     Knight,
     Pawn
 };
+
+vector<PieceType> allPieceTypes
+    { PieceType::King, PieceType::Queen, PieceType::Rook
+    , PieceType::Bishop, PieceType::Knight, PieceType::Pawn
+    };
+const Short PIECE_TYPES_COUNT = 6;
+
 ostream& operator<<(ostream& os, PieceType pt)
 {
     if (pt == PieceType::King)        { os << 'K'; }
@@ -34,65 +47,128 @@ ostream& operator<<(ostream& os, PieceType pt)
     return os;
 }
 
-const vector<PieceType> pieceTypes{
+using OptPieceType = std::optional<PieceType>;
+
+const vector<PieceType> pieceTypes {
     PieceType::King, PieceType::Queen, PieceType::Rook
     , PieceType::Bishop, PieceType::Knight, PieceType::Pawn
     };
 
+// ========================================
+// Piece
+
 class Board;
 
-class Piece {
+class Piece
+{
 public:
-    static PieceValue pieceValue(PieceType pt) { return _pieceType2PieceValue.at(pt); }
+    // Static method
+    static PieceValue pieceValue(PieceType pt);
 
-    Piece(Color color, PieceType pt, Short index)
-        : _color{color}, _pieceType{pt}, _pos{index},
-          _lastMoveIndex{0}
-        {}
+    // Constructor
+    Piece(Color color, PieceType pt, Short index, MoveIndex lastMoveIndex=0);
 
+    // Public read methods
     Color color() const { return _color; }
     bool  isBlack() const { return _color == Color::Black; }
     bool  isWhite() const { return _color == Color::White; }
 
     PieceType pieceType() const { return _pieceType; }
 
-    Pos   pos() const { return _pos; }
+    Short index() const { return _pos.x + _pos.y * BOARD_COLS; }
     Col   col() const { return _pos.x; }
     Row   row() const { return _pos.y; }
-    Short index() const { return _pos.x + _pos.y * BOARD_COLS; }
-    Short lastMoveIndex() const { return _lastMoveIndex; }
+    Pos   pos() const { return _pos; }
+    Color squareColor() const { return _pos.squareColor(); }
 
-    void moveTo(const Pos& pos) {
-        _pos.moveTo(pos);
-    }
+    bool hasMoved() const { return lastMoveIndex() > 0; }
+    MoveIndex lastMoveIndex() const;
+
+    // Public write methods
+    void moveTo(const Pos& pos) { _pos.moveTo(pos); }
+    void rollBackLastMoveIndex(MoveIndex mi);
     void setPieceType(PieceType pt) { _pieceType = pt; }
+    void updateMoveIndexHistory(MoveIndex mi);
 
+    // Operator
     bool operator<(const Piece& other) { return _pos < other.pos(); }
 
-    friend ostream& operator<<(ostream& os, const Piece& piece);
-
 private:
-    static map<PieceType, PieceValue> _pieceType2PieceValue;
-
     Color     _color;
     PieceType _pieceType;
     Pos       _pos;
-    Short     _lastMoveIndex;  // Convention: Zero before first move
+    MoveIndexHistory _moveIndexHistory;  // First move=1. Never moved=0.
 
-    friend struct std::hash<Board>;
+    friend ostream& operator<<(ostream& os, const Piece& piece);
 };
 
-ostream& operator<<(ostream& os, const Piece& piece) {
+// ---------- Static public method
+PieceValue Piece::pieceValue(PieceType pt)
+{
+    static map<PieceType, PieceValue> pt2pv{
+        {PieceType::King,     KING_VALUE}
+        , {PieceType::Queen,  9.0}
+        , {PieceType::Rook,   5.0}
+        , {PieceType::Bishop, 3.5}
+        , {PieceType::Knight, 3.0}
+        , {PieceType::Pawn,   1.0}
+    };
+    if (  pt != PieceType::King   && pt != PieceType::Queen
+       && pt != PieceType::Rook   && pt != PieceType::Bishop
+       && pt != PieceType::Knight && pt != PieceType::Pawn
+       )
+    {
+        logger.error("Piece::pieceValue: Called with unrecognized PieceType.");
+        return 0.0;  // TODO: Diagnose & remove workaround.
+    }
+    return pt2pv.at(pt);
+}
+
+// ---------- Constructors
+Piece::Piece(Color color, PieceType pt, Short index, MoveIndex lastMoveIndex /* =0 */)
+    : _color{color}, _pieceType{pt}, _pos{index},
+      _moveIndexHistory{}
+{
+    _moveIndexHistory.reserve(lastMoveIndex + VECTOR_CAPACITY_INCR);
+    assert(_moveIndexHistory.capacity() - 1 >= (unsigned long) lastMoveIndex);
+    if (lastMoveIndex == 0) {
+        _moveIndexHistory.push_back(true);
+    } else {
+        _moveIndexHistory[lastMoveIndex] = true;
+    }
+}
+
+// ---------- Public read methods
+MoveIndex Piece::lastMoveIndex() const
+{
+    for (int k = _moveIndexHistory.size(); k >= 0; --k) {
+        if (_moveIndexHistory[k]) { return k; }
+    }
+    return 0;
+}
+
+// ---------- Public write methods
+void Piece::rollBackLastMoveIndex(MoveIndex mi)
+{
+    const MoveIndexHistory::iterator& beg = _moveIndexHistory.begin() + mi;
+    const MoveIndexHistory::iterator& end = _moveIndexHistory.end();
+    _moveIndexHistory.erase(beg, end);
+}
+
+void Piece::updateMoveIndexHistory(MoveIndex mi)
+{
+    if ((_moveIndexHistory.capacity()) <= (unsigned long) mi + 1)
+    {
+        _moveIndexHistory.reserve(mi + VECTOR_CAPACITY_INCR);
+    }
+    assert(_moveIndexHistory.capacity() - 1 >= (unsigned long) mi);
+    _moveIndexHistory[mi] = true;
+}
+
+// ---------- Operator
+ostream& operator<<(ostream& os, const Piece& piece)
+{
     os << piece._color << piece._pieceType
        << "_@_" << piece._pos << "=index#" << piece.index();
     return os;
 }
-
-map<PieceType, PieceValue> Piece::_pieceType2PieceValue{
-    {PieceType::King, 1'000'000.0}
-    , {PieceType::Queen,  9.0}
-    , {PieceType::Rook,   5.0}
-    , {PieceType::Bishop, 3.5}
-    , {PieceType::Knight, 3.0}
-    , {PieceType::Pawn,   1.0}
-    };
