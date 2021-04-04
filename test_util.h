c1a94adf7b3a5d73014c1de3c2a2a534b32e0319
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:36:39 -0800

    Convert to using GoogleTest.

--- test_util.h
+++ test_util.h
@@ -1,5 +1,7 @@
 // Copyright 2021, by Jay M. Coskey
 
+#pragma once
+
 #include <iostream>
 #include <set>
 #include <sstream>
@@ -10,24 +12,23 @@
 using std::cout;
 
 
-int main() {
+TEST(UtilTest, UtilMisc) {
+    ScopedTracer(__func__);
     Dirs orthoDirs  = dirSignedPerms(Dir(1,0));
     Dirs diagDirs   = dirSigns(Dir(1,1));
     Dirs allDirs    = getUnion(orthoDirs, diagDirs);
 
     Dirs knightDirs = dirSignedPerms(Dir(1,2));
 
-    assert(orthoDirs.size() == 4);
-    assert(diagDirs.size()  == 4);
-    assert(allDirs.size()   == 8);
+    EXPECT_EQ(orthoDirs.size(),  (unsigned long) 4);
+    EXPECT_EQ(diagDirs.size(),   (unsigned long) 4);
+    EXPECT_EQ(allDirs.size(),    (unsigned long) 8);
 
-    assert(knightDirs.size() == 8);
+    EXPECT_EQ(knightDirs.size(), (unsigned long) 8);
 
-    cout << "orthoDirs="  << showSet(orthoDirs) << "\n";
-    cout << "diagDirs="   << showSet(diagDirs)  << "\n";
-    cout << "allDirs="    << showSet(allDirs)   << "\n";
-
-    cout << "knightDirs=" << showSet(knightDirs) << "\n";
-
-    return 0;
+    // cout << "orthoDirs="  << orthoDirs  << "\n";
+    // cout << "diagDirs="   << diagDirs   << "\n";
+    // cout << "allDirs="    << allDirs    << "\n";
+ 
+    // cout << "knightDirs=" << knightDirs << "\n";
 }
