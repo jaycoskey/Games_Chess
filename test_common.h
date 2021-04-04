7669cfb3d43fc546130162170e472d2f0b1cb267
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:38:18 -0800

    New common header file to support multiple tests (e.g., with easy custom Board setup)

---
+++ test_common.h
@@ -0,0 +1,123 @@
+// Copyright 2021, by Jay M. Coskey
+
+#pragma once
+
+#include <iostream>
+#include <string>
+
+#include "util.h"
+#include "piece.h"
+#include "board.h"
+
+
+class ScopedTracer {
+public:
+    ScopedTracer(const char* func_name, bool verbose=false)
+        : _func_name{func_name}, _verbose{verbose}
+    {
+        if (_verbose) {
+            cout << "Entering " << _func_name << "\n";
+        }
+    }
+    ~ScopedTracer() {
+        if (_verbose) {
+            cout << "Exiting " << _func_name << "\n";
+        }
+    }
+private:
+    const char* _func_name;
+    bool _verbose;
+};
+
+// bool equal_eps(float a, float b) {
+//     float eps = 10e-5;  // std::numeric_limits<float>::epsilon();
+//     return std::fabs(a - b) < eps;
+// }
+
+void add_bk_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::King,   pos, lmi);
+}
+void add_bq_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::Queen,  pos, lmi);
+}
+void add_br_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::Rook,   pos, lmi);
+}
+void add_bb_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::Bishop, pos, lmi);
+}
+void add_bn_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::Knight, pos, lmi);
+}
+void add_bp_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::Black, PieceType::Pawn,   pos, lmi);
+}
+
+void add_wk_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::King,   pos, lmi);
+}
+void add_wq_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::Queen,  pos, lmi);
+}
+void add_wr_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::Rook,   pos, lmi);
+}
+void add_wb_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::Bishop, pos, lmi);
+}
+void add_wn_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::Knight, pos, lmi);
+}
+void add_wp_to(Board& b, const string& pos, Short lmi=0) {
+    b.addPieceTo(Color::White, PieceType::Pawn,   pos, lmi);
+}
+
+// Black can castle Kingside & Queenside. White cannot castle on either side.
+Board mkCastlingBoard() {
+    Board b{false};
+
+    // Without specifying lastMoveIndex, pieces are treated as though they've never moved.
+    add_bk_to(b, "e8");
+    add_br_to(b, "a8");
+    add_br_to(b, "h8");
+    add_bb_to(b, "a6");
+    add_bb_to(b, "f6");
+
+    add_wk_to(b, "e1");
+    add_wr_to(b, "a1");
+    add_wr_to(b, "h1");
+    add_wp_to(b, "h2");
+
+    return b;
+}
+
+Board mkCheckmatesBoard() {
+    Board b{false};
+
+    // Piece constructor called via make_shared ignores lastMoveIndex argument.
+    // Workaround by updating MoveIndex history in a separate method call.
+    add_bk_to(b, "a8", 2); b.pieceAt(Pos("a8"))->updateMoveIndexHistory(2);
+    add_bq_to(b, "h3");
+    add_br_to(b, "h5");
+    add_bn_to(b, "f4");
+
+    add_bp_to(b, "b7");
+    add_bp_to(b, "e2");
+
+    add_wk_to(b, "h1", 2); b.pieceAt(Pos("h1"))->updateMoveIndexHistory(2);
+    add_wb_to(b, "e3");
+    add_wb_to(b, "e5");
+    add_wn_to(b, "d8");
+
+    add_wp_to(b, "a5");
+    add_wp_to(b, "c6");
+    add_wp_to(b, "f2");
+    add_wp_to(b, "g3");
+    add_wp_to(b, "h2");
+
+    return b;
+}
+
+void printHRule() {
+    cout << "----------------------------------------\n";
+}
