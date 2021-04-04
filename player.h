276cd3f66f3e213102de7160295251f024142b1a
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:27:26 -0800

    Add PlayerType (currently Human, Computer_Random, Computer_RandomCapture). Add method offerBool to offer Player a yes/no choice.

--- player.h
+++ player.h
@@ -2,27 +2,96 @@
 
 #pragma once
 
+#include <functional>
+#include <iostream>
+#include <map>
+#include <string>
+
 #include "util.h"
 #include "geometry.h"
 
 using std::string;
+using std::cin, std::cout;
 using std::map;
 
+using Color2Dir = map<Color, Dir>;
+using Color2Name = map<Color, string>;
 
-class Board;
+enum class PlayerType
+    { Human
+    , Computer_Random
+    , Computer_RandomCapture
+    };
 
-// TODO: Add user interactivity, including asking Players for their names
+using Color2PlayerType = map<Color, PlayerType>;
+
+
 class Player {
 public:
-    static const string& playerName(Color color) { return _color2PlayerName.at(color); }
-    static void setPlayerName(Color color, const string& name) { _color2PlayerName[color] = name; }
+    // ---------- Static read methods
+    static const Dir& backward(Color c);
+    static const Dir& forward(Color c);
 
-    static const Dir& getForward(Color color) { return _color2Forward.at(color); }
+    static string playerName(Color c) { return _color2PlayerName.at(c); }
+    static PlayerType playerType(Color c) { return _color2PlayerType.at(c); }
+
+    static bool offerBool(std::optional<Color> oc, const string& offerMsg);
+
+    // ---------- Static write methods
+    static void setPlayerName(Color c, string name) { _color2PlayerName[c] = name; }
+    static void setPlayerType(Color c, PlayerType pt) { _color2PlayerType[c] = pt; }
 
 private:
-    static map<Color, string> _color2PlayerName;
-    static const map<Color, Dir> _color2Forward;
+    static Color2Name       _color2PlayerName;
+    static Color2PlayerType _color2PlayerType;
 };
 
-map<Color, string> Player::_color2PlayerName{ {Color::Black, "Amy"}, {Color::White, "Bob"} };
-const map<Color, Dir> Player::_color2Forward{ {Color::Black, Dir{0, -1}}, {Color::White, Dir{0, 1}} };
+// ---------- Static data initialization
+Color2Name Player::_color2PlayerName
+    { {Color::Black, "Basho"}
+    , {Color::White, "Wilma"}
+    };
+
+Color2PlayerType Player::_color2PlayerType
+    { {Color::Black, PlayerType::Computer_Random}
+    , {Color::White, PlayerType::Computer_Random}
+    };
+
+// ---------- Static read methods
+const Dir& Player::backward(Color c)
+{
+    static const Color2Dir c2b
+        { {Color::Black, Dir{0,  1}}
+        , {Color::White, Dir{0, -1}}
+        };
+    return c2b.at(c);
+}
+
+const Dir& Player::forward(Color c)
+{
+    static const Color2Dir c2f
+        { {Color::Black, Dir{0, -1}}
+        , {Color::White, Dir{0,  1}}
+        };
+    return c2f.at(c);
+}
+
+bool Player::offerBool(std::optional<Color> oc, const string& offerMsg)
+{
+    if (oc != std::nullopt) {
+        cout << Player::playerName(*oc) << ": ";
+    }
+    cout << offerMsg;
+    while (true) {
+        string input;
+        getline(cin, input);
+        if (input.size() != 1) {
+            cout << "Please enter simply 'y' or 'n'.\n";
+            continue;
+        }
+        if (input != "y" && input != "n") {
+            continue;
+        }
+        return input == "y";
+    }
+}
