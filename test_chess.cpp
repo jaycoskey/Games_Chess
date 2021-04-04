91bf61e5f4a3de9fea8fc441c28fb48787342105
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:44:58 -0800

    Driver for all tests. All tests now use GoogleTest, except for tests of Logger: their output needs to be inspected, due to I/O.

---
+++ test_chess.cpp
@@ -0,0 +1,23 @@
+// Copyright 2021, by Jay M. Coskey
+
+#include <iostream>
+
+#include "test_common.h"
+
+#include "test_board.h"
+#include "test_board_state.h"
+#include "test_logger.h"
+#include "test_move.h"
+#include "test_util.h"
+
+using std::cout;
+
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    auto result = RUN_ALL_TESTS();
+    cout << "========================================\n";
+    test_logger();  // Check output
+
+    return result;
+}
