59dbb8da204160a8e92813735ec4cb35c5d4f640
Jay Coskey <jay.coskey@gmail.com>
2021-04-03 22:20:23 -0800

    Add some support for testing that is not used in the app itself (e.g., constructing Pos from string)

--- geometry.h
+++ geometry.h
@@ -12,7 +12,7 @@
 #include "util.h"
 
 using std::string;
-using std::cout, std::ostream;
+using std::ostream;
 using std::set;
 
 struct Dir;
@@ -21,16 +21,23 @@ using Dirs = set<Dir>;
 
 const Col BOARD_COLS = 8;
 const Row BOARD_ROWS = 8;
+const Short BOARD_SPACES = BOARD_COLS * BOARD_ROWS;
+
+const Col BOARD_KING_COL = 4;
+const Row BOARD_PAWN_PROMOTION_ROW = BOARD_ROWS - 1;
+const Row BOARD_EN_PASSANT_FROM_ROW = 4;
 
 // ---------- Board index inversion
 
-Short invertIndex(Short index) { return BOARD_ROWS * BOARD_COLS - 1 - index; }
+Row homeRow(Color c) { return c == Color::Black ? BOARD_ROWS - 1 : 0; }
+Short invertIndex(Short index) { return BOARD_SPACES - 1 - index; }
 Short invertRow(Short index) {
-    return BOARD_ROWS * BOARD_COLS - 1 - index
+    return BOARD_SPACES - 1 - index
         + 2 * (index % BOARD_COLS) - (BOARD_COLS - 1);
 }
 
-// ---------- Direction
+// ========================================
+// Direction
 
 struct Dir {
     Dir(Col x, Row y) : x{x}, y{y} {}
@@ -38,6 +45,7 @@ struct Dir {
     Col x;
     Row y;
 
+    bool isDir(Col dx, Row dy) { return dx == x && dy == y; }
     Dir operator+(const Dir& d) const { return Dir(x + d.x, y + d.y); }
     bool operator<(const Dir& other) const;
 
@@ -80,7 +88,7 @@ Dirs dirSignedPerms(const Dir& dir)
     return result;
 }
 
-// ---------- Direction non-member values
+// ---------- Direction-valued constants
 
 Dirs orthoDirs   = dirSignedPerms(Dir(1,0));
 Dirs diagDirs    = dirSigns(Dir(1,1));
@@ -88,44 +96,59 @@ Dirs allDirs     = getUnion(orthoDirs, diagDirs);
 
 Dirs knightDirs  = dirSignedPerms(Dir(1,2));
 
-// ---------- Position
+// ========================================
+// Position
 
 struct Pos {
     Pos(Col x, Row y) : x{x}, y{y} {}
     Pos(Short index) : Pos(div(index, BOARD_COLS)) {}
     Pos(div_t d) : x(d.rem), y(d.quot) {}
     Pos(const Pos& pos) : x(pos.x), y(pos.y) {}
+    Pos(const string& posStr);  // For testing
 
     Col x;
     Row y;
 
+    // ---------- Pos read methods
+    // Convert abs<-->rel
+    Pos fromRel(Color c) const { return Pos(toRelCol(c), toRelRow(c)); }
+    Col toRelCol(Color c) const { return c == Color::White ? x : BOARD_COLS - 1 - x; }
+    Row toRelRow(Color c) const { return c == Color::White ? y : BOARD_ROWS - 1 - y; }
+
+    // Shift left/right
+    Pos posLeft(Col col)  const { return Pos(x - col, y); }
+    Pos posRight(Col col) const { return Pos(x + col, y); }
+
+    const string algNotation() const;
+    Short index() const { return x + BOARD_COLS * y; }
+    bool  isAt(Col col, Row row) const { return col == x && row == y; }
+    bool  isOnBoard() const { return x >= 0 && y >= 0 && x < BOARD_COLS && y < BOARD_ROWS; }
+    bool  isPawnInitialPosition(Color color) const { return toRelRow(color) == 1; }
+    bool  isPawnPromotionRow(Color color) const {
+        return toRelRow(color) == BOARD_PAWN_PROMOTION_ROW;
+    }
+    Color squareColor() const { return (x + y) % 2 == 0 ? Color::Black : Color::White; }
+    Col   xdiff(const Pos& other) const { return x - other.x; }
+    Row   ydiff(const Pos& other) const { return y - other.y; }
+
+    // ---------- Pos write methods
+    void moveTo(const Pos& other) { x = other.x; y = other.y; }
+
+    // ---------- Pos operators
     Pos operator+(const Dir& d) const { return Pos(x + d.x, y + d.y); }
     bool operator<(const Pos& other) const;
-    bool operator==(const Pos& other) const;
-
-    Pos fromRel(Color c) const { return Pos(toRelCol(c), toRelRow(c)); }
-
-    Col   toRelCol(Color c) const { return c == Color::White ? x : BOARD_COLS - 1 - x; }
-    Row   toRelRow(Color c) const { return c == Color::White ? y : BOARD_ROWS - 1 - y; }
-    Short index() const { return x + BOARD_COLS * y; }
-    const string algNotation() const;
-
-    Short xdiff(const Pos& other) const { return other.x - x; }
-
-    Pos posLeft(Color c, Short n)  const { return Pos(x + (c == Color::White ? -n :  n), y); }
-    Pos posRight(Color c, Short n) const { return Pos(x + (c == Color::White ?  n : -n), y); }
-
-    void moveTo(const Pos& other);
+    bool operator==(const Pos& other) const { return x == other.x && y == other.y; }
 
     friend ostream& operator<<(ostream& os, const Pos& pos);
 };
 
-ostream& operator<<(ostream& os, const Pos& pos)
-{
-    os << pos.algNotation();
-    return os;
-}
+// ---------- Pos constructor
+Pos::Pos(const string& posStr)
+    : x{static_cast<Short>(tolower(posStr[0]) - 'a')}
+    , y{static_cast<Short>(stoi(posStr.substr(1)) - 1)}
+{}  // For testing
 
+// ---------- Pos read methods
 const string Pos::algNotation() const
 {
     ostringstream oss;
@@ -141,27 +164,14 @@ const string Pos::algNotation() const
     return oss.str();
 }
 
-// ---------- public Position methods
-
+// ---------- Pos operators
 bool Pos::operator<(const Pos& other) const
 {
-    if (x < other.x || (x == other.x && y < other.y)) { return true; }
-    return false;
+    return x < other.x || (x == other.x && y < other.y);
 }
 
-bool Pos::operator==(const Pos& other) const
+ostream& operator<<(ostream& os, const Pos& pos)
 {
-    return x == other.x && y == other.y;
-}
-
-void Pos::moveTo(const Pos& other)
-{
-    x = other.x;
-    y = other.y;
-}
-
-// ---------- Non-member Position methods
-
-bool isPawnInitialPosition(Color color, const Pos& pos) {
-    return pos.toRelRow(color) == 1;
+    os << pos.algNotation();
+    return os;
 }
